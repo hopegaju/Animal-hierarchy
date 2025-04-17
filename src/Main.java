30bcb7f3a2899731871b4a0c7800364cb736143a
hopegaju <hopegajupr@gmail.com>
2025-04-17 13:05:25 +0200

    created instances of dog, cat and bird and called makeSound() and fly() methods

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         Bird snow = new Bird();
+        Cat scoop = new Cat();
+        Dog sword = new Dog();
         snow.makeSound();
         snow.fly();
+        scoop.makeSound();
+        sword.makeSound();
     }
 }
