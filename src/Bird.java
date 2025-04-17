f54aaa2bc4d4b6c5b492b28d8fbb2b8fb10ab981
hopegaju <hopegajupr@gmail.com>
2025-04-17 12:54:58 +0200

    added methods fly() and override makeSound()

--- src/Bird.java
+++ src/Bird.java
@@ -1,3 +1,9 @@
 public class Bird extends Animal {
-
+    public void fly() {
+        System.out.println("Birds do Fly!");
+    }
+    @Override
+    public void makeSound() {
+        System.out.println("Bird says: Chirp!");
+    }
 }
