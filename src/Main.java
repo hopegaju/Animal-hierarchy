708fa674be64e5d698a92e1e6476cefdbf95e152
hopegaju <hopegajupr@gmail.com>
2025-04-17 12:49:52 +0200

    Added parent class and subclass

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Bird snow = new Bird();
+        snow.makeSound();
+        snow.fly();
     }
 }
